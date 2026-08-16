6357599e356cae5ba641f51460c306eeffd77fa3
hrittik702 <mmhrittik@gmail.com>
2026-08-16 08:15:26 +0530

    Database Connection setup

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+const listing = require('./models/listings');
 const port = 3000;
 const app = express();
 
@@ -10,11 +11,27 @@ app.set('views', path.join(__dirname, '/views'));
 app.set('public', path.join(__dirname, '/public'));
 app.use(express.static('public'));
 
+// connecting to database
+const main = async () => {
+  await mongoose.connect('mongodb://127.0.0.1:27017/stayly');
+};
+
+main()
+  .then((res) => {
+    console.log('Database Stayly : Connected');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 // server listening
 app.listen(port, () => {
   console.log('Server Active');
 });
 
-app.get('/', (req, res) => {
-  res.render('stayly.ejs');
+// index route
+app.get('/', async (req, res) => {
+  const listings = await listing.find();
+  res.render('stayly.ejs', {listings});
 });
+
