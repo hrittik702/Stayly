004023180c45c722c28b16f7328c12e14e922575
hrittik702 <mmhrittik@gmail.com>
2026-08-17 10:11:49 +0530

    Listing.js add

--- models/listings.js
+++ models/listings.js
@@ -9,13 +9,11 @@ const listingSchema = new mongoose.Schema({
     type: String,
   },
   image: {
-    type: String,
-    default:
-      'https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60',
-    set: (v) => {
-      v === ''
-        ? 'https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60'
-        : v;
+    filename: {
+      type: String,
+    },
+    url: {
+      type: String,
     },
   },
   price: {
@@ -32,3 +30,4 @@ const listingSchema = new mongoose.Schema({
 const listing = mongoose.model('listing', listingSchema);
 
 module.exports = listing;
+  
