67f8ddf14894f2dd6aa8741be45da1b7a020fc4f
hrittik702 <mmhrittik@gmail.com>
2026-08-26 12:01:42 +0530

    UI Done

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const ejsMate = require('ejs-mate');
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 app.set('public', path.join(__dirname, '/public'));
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.engine('ejs', ejsMate);
