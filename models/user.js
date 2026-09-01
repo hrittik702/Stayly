90a8cb8ee1a0301cb180b2d4e37c9172ae46f72c
hrittik702 <mmhrittik@gmail.com>
2026-09-01 08:15:11 +0530

    Add model : user.js (user informations)

--- models/user.js
+++ models/user.js
@@ -1,23 +1,15 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const passportLocalMongoose = require('passport-local-mongoose');
 
 const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-  },
   email: {
     type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  role: {
-    type: String,
-    default: 'client',
     required: true,
   },
 });
 
 const user = mongoose.model('user', userSchema);
+user.plugin(passportLocalMongoose);
+
+module.exports = user;
